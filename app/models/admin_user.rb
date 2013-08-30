72e36cd178daada270e2dda53dee7e506527e37a
Michal Wrobel <sparrovv@gmail.com>
2013-08-30 16:06:09 +0100

    Don't allow to signup for admin page

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -2,6 +2,5 @@ class AdminUser < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 end
