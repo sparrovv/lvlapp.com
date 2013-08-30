464240fd25f667e064b8a14512941e96c4f725fb
Michal Wrobel <sparrovv@gmail.com>
2013-08-30 15:18:37 +0100

    Don't compress js

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
+default_run_options[:shell] = '/bin/bash -l'
 require 'bundler/capistrano'
 require 'capistrano_colors'
 
 load 'deploy/assets'
-
 set :stages, ['production']
 set :default_stage, 'production'
 require 'capistrano/ext/multistage'
@@ -11,9 +11,11 @@ set :application, "landl"
 set :deploy_to, "/home/sparrovv/#{application}"
 set :scm, :git
 set :repository, "git@gitlab.com:sparrovv/landl.git"
+set :deploy_via, :remote_cache
 
 set :branch, ENV['branch'] || :master
 set :use_sudo, false
+set :keep_releases, 5
 
 namespace :r do
   desc "Run a task on a remote server.
