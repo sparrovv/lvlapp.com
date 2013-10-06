49ca0c3e911c997ad23a19679414ac66a8396c36
Michal Wrobel <sparrovv@gmail.com>
2013-10-06 11:58:43 +0100

    Redesign ;-)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,10 @@
 class HomeController < ApplicationController
+  layout 'front'
+
+  def index
+    @recent_videos = AudioVideo.
+      by_status(AudioVideo::ACTIVE).
+      order('created_at desc').limit(8)
+  end
 
 end
