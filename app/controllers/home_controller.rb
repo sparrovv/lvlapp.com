fbc49b05325f9a3ef83698b81ef478c3d3423803
Michal Wrobel <sparrovv@gmail.com>
2013-11-23 15:50:20 +0000

    Up to 12 videos on main site

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,16 +4,16 @@ class HomeController < ApplicationController
   def index
     @recent_videos = AudioVideo.
       by_status(AudioVideo::ACTIVE).
-      order('created_at desc').limit(10)
+      order('created_at desc').limit(12)
 
     @popular_videos = AudioVideo.
       by_status(AudioVideo::ACTIVE).
-      order('views_count desc').limit(10)
+      order('views_count desc').limit(12)
 
     @featured_videos = AudioVideo.
       featured.
       by_status(AudioVideo::ACTIVE).
-      order('created_at desc').limit(10)
+      order('created_at desc').limit(12)
   end
 
 end
